d9c4f6873808e3d34f42f62fcacb2cdf12c04133
Richard <richie765@gmail.com>
2016-06-28 10:40:18 +0200

    Mockup using new module

--- examples/leaderboard/server/publish.js
+++ examples/leaderboard/server/publish.js
@@ -1,16 +1,7 @@
+// import PgSelect from 'meteor-pg-live';
+import PgSelect from '../imports/pg-live';
+
 Meteor.publish('allPlayers', function() {
-  var self = this;
-
-  var players = [
-    { name: 'Kepler', score: 40 },
-    { name: 'Leibniz', score: 50 },
-    { name: 'Maxwell', score: 60 },
-    { name: 'Planck', score: 70 },
-  ];
-
-  _.each(players, function(player) {
-    self.added('players', Random.id(), player);
-  });
-
-  self.ready();
+  let sql = 'SELECT * FROM players ORDER BY score DESC';
+  PgSelect(this, 'players', sql);
 });
