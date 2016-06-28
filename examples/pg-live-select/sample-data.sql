d2e17d736ce43e8a9b3b905f828a6bd719846771
Richard <richie765@gmail.com>
2016-06-28 07:53:58 +0200

    Use FORMAT CSV as editors tend to replace tabs with spaces

--- examples/pg-live-select/sample-data.sql
+++ examples/pg-live-select/sample-data.sql
@@ -51,35 +51,34 @@ ALTER TABLE ONLY scores ALTER COLUMN id SET DEFAULT nextval('scores_id_seq'::reg
 
 ALTER TABLE ONLY students ALTER COLUMN id SET DEFAULT nextval('students_id_seq'::regclass);
 
-COPY assignments (id, class_id, name, value) FROM stdin;
-1 1 Homework  10
-2 1 Test  100
-3 2 Art Project 30
-4 1 HW 2  10
-5 1 HW 3  10
-6 1 HW 4  10
+COPY assignments (id, class_id, name, value) FROM stdin (FORMAT CSV);
+1,1,Homework,10
+2,1,Test,100
+3,2,Art Project,30
+4,1,HW 2,10
+5,1,HW 3,10
+6,1,HW 4,10
 \.
-
 
 SELECT pg_catalog.setval('assignments_id_seq', 6, true);
 
-COPY scores (id, assignment_id, student_id, score) FROM stdin;
-1 1 1 9
-2 1 2 8
-3 2 1 75
-4 2 2 77
-5 2 3 50
-6 3 1 20
-10  4 1 7
-11  5 1 8
+COPY scores (id, assignment_id, student_id, score) FROM stdin (FORMAT CSV);
+1,1,1,9
+2,1,2,8
+3,2,1,75
+4,2,2,77
+5,2,3,50
+6,3,1,20
+10,4,1,7
+11,5,1,8
 \.
 
 SELECT pg_catalog.setval('scores_id_seq', 11, true);
 
-COPY students (id, name) FROM stdin;
-1 John Doe
-2 Larry Loe
-3 Oklahoma
+COPY students (id, name) FROM stdin (FORMAT CSV);
+1,John Doe
+2,Larry Loe
+3,Oklahoma
 \.
 
 
@@ -93,5 +92,3 @@ ALTER TABLE ONLY scores
 
 ALTER TABLE ONLY students
     ADD CONSTRAINT students_pkey PRIMARY KEY (id);
-
-
