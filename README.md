b6c11eb3144fbc4bfe19b4384a727930755547f2
Richard <richie765@gmail.com>
2016-07-02 07:34:15 +0200

    ecmascript

--- README.md
+++ README.md
@@ -16,6 +16,12 @@ object in your methods to run UPDATE and INSERT statements.
 meteor npm install meteor-pg-live --save
 ```
 
+You'll need the ecmascript package, so install it if you don't have it yet
+
+```bash
+meteor add ecmascript
+```
+
 If you want to embed a PostgreSQL server into your application, you can use
 [numtel/meteor-pg-server](https://github.com/numtel/meteor-pg-server). To
 install:
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var LivePg = require('pg-live-select');
-var pgp = require('pg-promise')();
+import LivePg from 'pg-live-select';
+import pgPromise from 'pg-promise';
 
 // Update this connection string to match your configuration!
 // When using an externally configured PostgreSQL server, the default port
@@ -7,12 +7,13 @@ var pgp = require('pg-promise')();
 
 // TODO Use environment variables and proper defaults
 
-var PG_URL = process.env.PG_URL ? process.env.PG_URL : 'postgres://' + process.env.USER + ':numtel@127.0.0.1:5438/postgres';
-var PG_CHANNEL = process.env.PG_CHANNEL ? process.env.PG_CHANNEL : 'default_channel';
+let PG_URL = process.env.PG_URL ? process.env.PG_URL : 'postgres://' + process.env.USER + ':numtel@127.0.0.1:5438/postgres';
+let PG_CHANNEL = process.env.PG_CHANNEL ? process.env.PG_CHANNEL : 'default_channel';
 
 // pg-promise connection
 
-var db = pgp(PG_URL);
+let pgp = pgPromise({});
+let db = pgp(PG_URL);
 
 try {
   console.log('meteor-pg: connecting to', PG_URL);
@@ -26,9 +27,9 @@ catch(err) {
 
 // liveDb connection
 
-var liveDb = new LivePg(PG_URL, PG_CHANNEL);
+let liveDb = new LivePg(PG_URL, PG_CHANNEL);
 
-var closeAndExit = function() {
+let closeAndExit = function() {
   // Cleanup removes triggers and functions used to transmit updates
   liveDb.cleanup(process.exit);
 };
@@ -41,19 +42,19 @@ process.on('SIGINT', closeAndExit);
 // select function
 
 function live_select(sub, collection, query, params, triggers) {
-  var initial = true;
-  var oldIds = [];
+  let initial = true;
+  let oldIds = [];
 
-  var query = liveDb.select(query, params, triggers)
+  let handle = liveDb.select(query, params, triggers)
     .on('update', function(diff, data) {
       // console.log('diff', diff);
       // console.log('data', data);
 
       // Get id's and check uniqueness
 
-      var newIds = data.map(row => row._id);
+      let newIds = data.map(row => row._id);
 
-      var check = {};
+      let check = {};
       newIds.forEach(id => {
         if(id === null || id === undefined) throw new Meteor.Error('meteor-pg: record without _id');
         if(check[id]) throw new Meteor.Error('meteor-pg: duplicate _id in resultset');
@@ -73,19 +74,19 @@ function live_select(sub, collection, query, params, triggers) {
         diff.added.forEach(function(added) {
           // Get _id
 
-          var _id = added._id;
+          let _id = added._id;
           if(!_id) throw new Meteor.Error('meteor-pg: each record must have an _id field');
 
           // Create copy of the record minus technical fields
 
-          var copy = _.clone(added);
+          let copy = _.clone(added);
           delete copy._index;
           delete copy._hash;
           delete copy._id;
 
           // Use 'changed' if it existed before, othewise 'added'
 
-          var index = oldIds.findIndex(newId => newId === _id);
+          let index = oldIds.findIndex(newId => newId === _id);
 
           if(index >= 0) {
             sub.changed(collection, _id, copy);
@@ -104,12 +105,12 @@ function live_select(sub, collection, query, params, triggers) {
         diff.removed.forEach(function(removed) {
           // Get _id
 
-          var _id = oldIds[removed._index - 1];
+          let _id = oldIds[removed._index - 1];
           if(!_id) throw new Meteor.Error('meteor-pg: can\'t remove a record we didn\'t see before');
 
           // Skip if it's still in our dataset
 
-          var index = newIds.findIndex(newId => newId === _id);
+          let index = newIds.findIndex(newId => newId === _id);
 
           if(index === -1) {
             sub.removed(collection, _id);
@@ -132,7 +133,7 @@ function live_select(sub, collection, query, params, triggers) {
 
   sub.onStop(function() {
     // console.log("Subscription was stopped");
-    query.stop();
+    handle.stop();
   });
 }
 
