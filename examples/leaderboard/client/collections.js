ba5b78e72100ced5ef4c68b9a747774da38e0d86
Richard <richie765@gmail.com>
2016-07-21 11:59:50 +0200

    Moved leaderboard example to own repo

--- examples/leaderboard/client/collections.js
+++
@@ -1,5 +0,0 @@
-// Define these collections only on the client-side
-// as on the server the data isn't coming from Mongo.
-
-Players = new Mongo.Collection('players');
-PlayerScore = new Mongo.Collection('playerScore');
--- examples/leaderboard/client/methods.js
+++
@@ -1,7 +0,0 @@
-// Provide a client side stub for latency compensation
-
-Meteor.methods({
-  'incScore': function(id, amount){
-    Players.update(id, { $inc: { score: amount } });
-  }
-});
